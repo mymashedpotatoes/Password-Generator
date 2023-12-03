2d0eae92385a9dbaa653005fec94bac2d41f1893
ella gaul <145066673+mymashedpotatoes@users.noreply.github.com>
2023-12-03 13:41:32 -0600

    updated js

--- script.js
+++ script.js
@@ -16,22 +16,24 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//arrays for password
 var uppercaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var lowercaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var numbersArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 var specialArray = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '=', '+', '[', ']', '{', '}', '<', '>', '?'];
 
+//starts with empty array and password length
 var choicesArray = [];
 passwordLength = "";
 
-
+//prompts for user input
 var userInput = function() {
   passwordLength = window.prompt("How many characters would you like the password to be? (8-128 characters)");
     if (passwordLength < 8 || passwordLength > 128) {
       alert("Character length must be between 8 and 128 characters. Please try again.")
       choicesArray = [];
       console.log(passwordLength)
-      return false;
+      return " "
     }
   var userUppercase = window.confirm("Would you like to add uppercase letters to the password?");
     if (userUppercase) {
@@ -53,11 +55,12 @@ var userInput = function() {
       choicesArray = choicesArray.concat(specialArray);
       console.log(userSpecial)
     } 
-  if (!userUppercase + !userLowercase + !userNumbers + !userSpecial) {
-    choicesArray = [];
-    alert("Must choose at least 1 character type.")
+    //if all choices are cancelled then will alert
+  if (!userUppercase && !userLowercase && !userNumbers && !userSpecial) {
+    alert("Must choose at least 1 character type.");
+    return " "
   }
-  return true;
+  return " ";
   
 }
 
